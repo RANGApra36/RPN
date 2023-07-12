5b3ae89cb79c5bffeb4d125666e401dab5a009aa
RANGApra36 <rp000341@gmail.com>
2023-07-12 06:22:35 +0530

    ArrayDemo5.java

---
+++ PolyDemo3.java
@@ -0,0 +1,40 @@
+package javaDemo;
+
+public class PolyDemo3 
+{
+
+	public static void main(String[] args) 
+	{
+		PolyDemo3 p1=new PolyDemo3();
+		A a1=new A();
+		//p1.funA(A a1);
+		//p1.funA(B b1);
+		//p1.funA(C c1);
+		A k=new A();
+		p1.funA(k);
+		p1.funA(new A());
+		
+		B l=new B();
+		p1.funA(l);
+		p1.funA(new B());
+		
+		C n=new C();
+		p1.funA(n);
+		p1.funA(new C());
+
+}
+		void funA(A a1)
+		{
+			System.out.println("class A args of PolyDemo3");
+		}
+		void funA(B b1)
+		{
+			System.out.println("class B args of PolyDemo3");
+		}
+		void funA(C c1)
+		{
+			System.out.println("class C args of PolyDemo3");
+		}
+	}
+
+
